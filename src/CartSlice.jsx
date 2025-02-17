2d4c7d225ce0617bc14be8406f98e71a15fefd04
Selene Echartea <168042467+selemist@users.noreply.github.com>
2025-02-17 09:50:30 -0300

    First commit
    Aquí dejo los cambios en CartItem.jsx , CartSlice.jsx y ProductList.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,21 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-    
+    name: 'cart',
+    initialState: {
+        items: [], // Initialize items as an empty array
     },
-    removeItem: (state, action) => {
+    reducers: {
+        addItem: (state, action) => {
+            const itemIndex = state.items.findIndex(item => item.name === action.payload);
+            if (itemIndex >= 0) {
+                state.items[itemIndex].quantity += 1;
+            } else {
+                state.items.push([...action.payload, quantity = 1]);
+            }
+        },
+        removeItem: (state, action) => {
+            state.items = state.items.filter(item => item.name !== action.payload.name);
+        },
+        updateQuantity: (state, action) => {
+            const item = state.items.find(item => item.name === action.payload.name);
+            if (item) {
+                item.quantity = action.payload.quantity;
+            }
+        },
     },
-    updateQuantity: (state, action) => {
-
-    
-    },
-  },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
